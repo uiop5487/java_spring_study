41b8bebc1291a4af3ae18442ea6911c9c6c1aac6
uiop5487 <uiop5487@gmail.com>
2022-10-14 22:46:28 +0900

    java 기본 변수 할당

---
+++ src/main/java/com/sparta/week01/prac/Prac.java
@@ -0,0 +1,22 @@
+package com.sparta.week01.prac;
+
+public class Prac {
+
+    public static void main(String[] args) {
+        // 문자열
+        String myName = "김진성";
+        System.out.println(myName);
+
+        // 정수
+        int age = 24;
+        System.out.println(age);
+
+        // 소수점
+        float num = 3.7f;
+        System.out.println(num);
+
+        // 100억 보다 큰 숫자
+        Long bigNum = 10000000000L;
+        System.out.println(bigNum);
+    }
+}
