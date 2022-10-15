ce726e0fe3f565bb9f2d0c308b724843974ef49d
uiop5487 <uiop5487@gmail.com>
2022-10-15 23:54:27 +0900

    java loop

---
+++ src/main/java/com/sparta/week01/prac/PracLoop.java
@@ -0,0 +1,25 @@
+package com.sparta.week01.prac;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PracLoop {
+
+    public static void main(String[] args) {
+        List<String> fruits = new ArrayList<>();
+        fruits.add("감");
+        fruits.add("배");
+        fruits.add("감");
+        fruits.add("딸기");
+        fruits.add("수박");
+        fruits.add("메론");
+        fruits.add("수박");
+        fruits.add("딸기");
+        fruits.add("메론");
+        fruits.add("수박");
+        fruits.add("메론");
+        fruits.add("수박");
+        fruits.add("감");
+        System.out.println(fruits);
+    }
+}
